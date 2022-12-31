7c179a478e9e9d37800ebfb334c950f0cfae72cb
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2023-01-01 00:03:08 +0530

    Homepage completed

--- sport-events/src/Pages/Homepage.jsx
+++ sport-events/src/Pages/Homepage.jsx
@@ -10,10 +10,21 @@ export const Homepage = () => {
   },[]);
 
   const getEvents = ()=>{
-    axios.get("")
+    axios.get("https://sport-event.onrender.com/events")
+    .then(res=>setEvents(res.data.events))
   }
 
   return (
-    <div>Homepage</div>
+    <div className="mainDiv">
+      {events.map((event)=>(
+        <div key={event._id} className="eventDiv">
+          <img className='eventImg' src={event.category === "footbal" ? "https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg?cs=srgb&dl=pexels-pixabay-46798.jpg&fm=jpg" : "https://static.toiimg.com/thumb/msid-85096936,width-1200,height-900,resizemode-4/.jpg"} alt="" />
+          <h1>{event.title}</h1>
+          <p>{event.category}</p>
+          <p>{event.location}</p>
+          <p>{event.startTime}</p>
+        </div>
+      ))}
+    </div>
   )
 }
